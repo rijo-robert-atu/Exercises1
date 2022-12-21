faf8ee69af11161e264d39d6919b7ee14819d3ab
rijo.robert <l00170978@atu.ie>
2022-12-21 02:27:18 +0000

    thrid commit

---
+++ git.bat
@@ -0,0 +1,21 @@
+#!/bin/bash
+# by: Rijo
+# Date: 21DEC22
+# Function: Perform a commit
+# Script: AddCommitPush.sh
+clear
+git status
+echo '**************************************************'
+echo "Performing an add for all files in this directory"
+git add .
+git status
+echo '**************************************************'
+echo 'Enter the commit message:'
+read CommitMessage
+git commit -m "$CommitMessage"
+git status
+echo '**************************************************'
+echo 'Pushing to GITHUB repository'
+git push -u origin master
+echo '**************************************************'
+echo 'Done!'
